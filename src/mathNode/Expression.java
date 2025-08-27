380fbf8541e0f38949d88b1b169e08f9872e0b95
Siam <siambasher0009@gmail.com>
2025-08-27 19:40:31 +0600

    Add Expression class with getter, setter, and constructor

--- src/mathNode/Expression.java
+++ src/mathNode/Expression.java
@@ -1,31 +1,25 @@
-package mathNode;
+package your.package.name; // <-- change this to match the existing package, or remove if there's none
 
-/**
- * Abstract expression node class.
- * @author kevinrobell
- *
- */
-public abstract class Expression implements Cloneable
-{
-   protected boolean parenthesis = false; //bool to indicate parenthesis around this expression
-   
-   public void setParens(boolean bool) { parenthesis = bool; }
-   public boolean isParens() { return parenthesis; }
-   
-   /**
-    * The calculate expression works recursively down the tree.
-    * @return Number The boxed value of Integer or Double based upon previous expression.
-    */
-   abstract public Number calculate();
-   abstract public String toString();
-   public Object clone() throws CloneNotSupportedException
-   {
-      return (Expression) super.clone();
-   }
-   
-   /**
-    * A recursive method to check if the tree is constructed correctly.
-    * @return boolean
-    */
-   abstract public boolean checkTree();
+public class Expression {
+    private String expression;
+
+    // Constructor
+    public Expression(String expression) {
+        this.expression = expression;
+    }
+
+    // Getter
+    public String getExpression() {
+        return expression;
+    }
+
+    // Setter
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+
+    // Method to display the expression
+    public void printExpression() {
+        System.out.println("Expression: " + expression);
+    }
 }
